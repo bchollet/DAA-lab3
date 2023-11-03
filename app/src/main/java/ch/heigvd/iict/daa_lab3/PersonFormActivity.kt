0493cccc5b7c49c6b8b0192cb3172d8c4bbcbb02
bchollet <babacouda@gmail.com>
2023-11-03 17:23:49 +0100

    feat: 🎉 init project

---
+++ app/src/main/java/ch/heigvd/iict/daa_lab3/PersonFormActivity.kt
@@ -0,0 +1,11 @@
+package ch.heigvd.iict.daa_lab3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PersonFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
