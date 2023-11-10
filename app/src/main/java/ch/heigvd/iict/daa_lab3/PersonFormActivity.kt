b2b97c2e67d9960811ad618266b14496d6c06f21
yvan.cochet <yvan.cochet@heig-vd.ch>
2023-11-10 18:39:10 +0100

    Set activity processes (fill form, empty form, create person from form)

--- app/src/main/java/ch/heigvd/iict/daa_lab3/PersonFormActivity.kt
+++ app/src/main/java/ch/heigvd/iict/daa_lab3/PersonFormActivity.kt
@@ -3,9 +3,14 @@ package ch.heigvd.iict.daa_lab3
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import ch.heigvd.iict.daa_lab3.databinding.ActivityPersonFormBinding
+import ch.heigvd.iict.daa_lab3.utils.Person
+import ch.heigvd.iict.daa_lab3.utils.Student
+import ch.heigvd.iict.daa_lab3.utils.Worker
+import java.util.Calendar
 
 /**
  * @author Cochet Yvan, Anthonponrajkumar Pirakasraj, Chollet Bastian
@@ -14,6 +19,7 @@ import ch.heigvd.iict.daa_lab3.databinding.ActivityPersonFormBinding
  */
 class PersonFormActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPersonFormBinding
+    private var calendar: Calendar = Calendar.getInstance()
 
     /**
      * @brief wrapper permettant l'utilisation d'une visibilité en un type
@@ -32,6 +38,9 @@ class PersonFormActivity : AppCompatActivity() {
         binding = ActivityPersonFormBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Fill form with example Person
+        fillForm(Person.exampleWorker);
+
         binding.mainBaseOccupationRdg.setOnCheckedChangeListener { _, choiceId ->
             when (choiceId) {
                 R.id.rdb_student -> {
@@ -49,6 +58,14 @@ class PersonFormActivity : AppCompatActivity() {
                 MotionEvent.ACTION_DOWN -> {true}
                 else -> {false}
             }
+        }
+
+        binding.okBtn.setOnClickListener{
+            Log.d("Person created",createPerson().toString())
+        }
+
+        binding.cancelBtn.setOnClickListener{
+            emptyForm();
         }
     }
 
@@ -70,5 +87,77 @@ class PersonFormActivity : AppCompatActivity() {
         setVisibility(binding.mainSpecificSectorSpinner, workerView)
         setVisibility(binding.mainSpecificExperienceTitle, workerView)
         setVisibility(binding.mainSpecificExperienceInput, workerView)
+    }
+
+    private fun fillForm(person: Person?){
+        if(person == null){
+            return
+        }
+
+        //Fill common values
+        binding.mainBaseNameInput.setText(person.name)
+        binding.mainBaseFirstnameInput.setText(person.firstName)
+        calendar = person.birthDay
+        //TODO : binding.mainBaseBirthdateInput.setText(person.birthDay)
+        binding.mainBaseNationalitySpinner.setSelection(resources.getStringArray(R.array.nationalities).indexOf(person.nationality))
+        binding.additionalEmailInput.setText(person.email)
+        binding.additionalRemarksInput.setText(person.remark)
+
+        //Fill worker specific values
+        if(person is Worker){
+            val worker = person as Worker
+            binding.mainSpecificCompagnyInput.setText(worker.company)
+            binding.mainSpecificSectorSpinner.setSelection(resources.getStringArray(R.array.sectors).indexOf(worker.sector))
+            binding.mainSpecificExperienceInput.setText(worker.experienceYear)
+        }
+        //Fill student specific values
+        if(person is Student){
+            val student = person as Student
+            binding.mainSpecificSchoolInput.setText(student.university)
+            binding.mainSpecificGraduationyearInput.setText(student.graduationYear)
+        }
+    }
+
+    private fun emptyForm(){
+        binding.mainBaseNameInput.setText("")
+        binding.mainBaseFirstnameInput.setText("")
+        binding.mainBaseBirthdateInput.setText("")
+        binding.mainBaseNationalitySpinner.setSelection(0)
+        binding.mainSpecificCompagnyInput.setText("")
+        binding.mainSpecificSectorSpinner.setSelection(0)
+        binding.mainSpecificExperienceInput.setText("")
+        binding.additionalEmailInput.setText("")
+        binding.additionalRemarksInput.setText("")
+        binding.mainSpecificSchoolInput.setText("")
+        binding.mainSpecificGraduationyearInput.setText("")
+    }
+
+    private fun createPerson() : Person? {
+        if(binding.rdbWorker.isChecked){
+            return Worker(
+                binding.mainBaseNameInput.toString(),
+                binding.mainBaseFirstnameInput.toString(),
+                calendar,
+                binding.mainBaseNationalitySpinner.selectedItem.toString(),
+                binding.mainSpecificCompagnyInput.toString(),
+                binding.mainSpecificSectorSpinner.selectedItem.toString(),
+                binding.mainSpecificExperienceInput.text.toString().toInt(),
+                binding.additionalEmailInput.toString(),
+                binding.additionalRemarksInput.toString()
+            )
+        }
+        else if(binding.rdbStudent.isChecked){
+            return Student(
+                binding.mainBaseNameInput.toString(),
+                binding.mainBaseFirstnameInput.toString(),
+                calendar,
+                binding.mainBaseNationalitySpinner.selectedItem.toString(),
+                binding.mainSpecificSchoolInput.toString(),
+                binding.mainSpecificGraduationyearInput.text.toString().toInt(),
+                binding.additionalEmailInput.toString(),
+                binding.additionalRemarksInput.toString()
+            )
+        }
+        return null
     }
 }
